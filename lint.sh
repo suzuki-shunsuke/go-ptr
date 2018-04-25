fbb4ab860503579d4dff1500dfe89361a717ccb0
Suzuki Shunsuke <suzuki-shunsuke@users.noreply.github.com>
2018-04-25 12:25:00 +0900

    ci: use gometalinter (#5)
    https://github.com/alecthomas/gometalinter

--- lint.sh
+++ lint.sh
@@ -1,14 +1,8 @@
 set -e
 
 echo "+ gofmt"
-! git ls-files | grep .go | xargs gofmt -s -d | grep '^'
-echo "+ go vet"
-go vet $(go list ./... | grep -v /vendor/)
-echo "+ golint"
-golint ./...
-echo "+ gosimple"
-! gosimple ./... | grep '^'
-echo "+ unused"
-unused ./...
+! git ls-files | grep ".go$" | xargs gofmt -s -d | grep '^'
+echo "+ gometalinter"
+gometalinter ./...
 echo "+ staticcheck (failure is ignored)"
 staticcheck ./... || echo "staticcheck failure is ignored"
