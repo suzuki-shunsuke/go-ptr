dc758e850b582f1f747cec2119b9209f847cfe67
Suzuki Shunsuke <suzuki-shunsuke@users.noreply.github.com>
2018-04-25 20:18:15 +0900

    ci: fix lint (#6)

--- lint.sh
+++ lint.sh
@@ -1,8 +1,6 @@
-set -e
-
 echo "+ gofmt"
-! git ls-files | grep ".go$" | xargs gofmt -s -d | grep '^'
+(! go list -f "{{.Dir}}" ./... | xargs gofmt -s -d | grep '^') || exit 1
 echo "+ gometalinter"
-gometalinter ./...
+gometalinter ./... || exit 1
 echo "+ staticcheck (failure is ignored)"
 staticcheck ./... || echo "staticcheck failure is ignored"
