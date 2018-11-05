24bedea0369ed5d7d3212121b16473f44059e702
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-11-05 11:40:10 +0900

    feat: add supported types

--- ptr.go
+++ ptr.go
@@ -10,7 +10,82 @@ func PInt(s int) *int {
 	return &s
 }
 
+// PUint returns a pointer of a given uint.
+func PUint(s uint) *uint {
+	return &s
+}
+
 // PBool returns a pointer of a given bool.
 func PBool(s bool) *bool {
+	return &s
+}
+
+// PUint8 returns a pointer of a given uint8.
+func PUint8(s uint8) *uint8 {
+	return &s
+}
+
+// PUint16 returns a pointer of a given uint16.
+func PUint16(s uint16) *uint16 {
+	return &s
+}
+
+// PUint32 returns a pointer of a given uint32.
+func PUint32(s uint32) *uint32 {
+	return &s
+}
+
+// PUint64 returns a pointer of a given uint64.
+func PUint64(s uint64) *uint64 {
+	return &s
+}
+
+// PInt8 returns a pointer of a given int8.
+func PInt8(s int8) *int8 {
+	return &s
+}
+
+// PInt16 returns a pointer of a given int16.
+func PInt16(s int16) *int16 {
+	return &s
+}
+
+// PInt32 returns a pointer of a given int32.
+func PInt32(s int32) *int32 {
+	return &s
+}
+
+// PInt64 returns a pointer of a given int64.
+func PInt64(s int64) *int64 {
+	return &s
+}
+
+// PFloat32 returns a pofloater of a given float32.
+func PFloat32(s float32) *float32 {
+	return &s
+}
+
+// PFloat64 returns a pofloater of a given float64.
+func PFloat64(s float64) *float64 {
+	return &s
+}
+
+// PComplex64 returns a pocomplexer of a given complex64.
+func PComplex64(s complex64) *complex64 {
+	return &s
+}
+
+// PComplex128 returns a pocomplexer of a given complex128.
+func PComplex128(s complex128) *complex128 {
+	return &s
+}
+
+// PRune returns a pocomplexer of a given rune.
+func PRune(s rune) *rune {
+	return &s
+}
+
+// PByte returns a pocomplexer of a given byte.
+func PByte(s byte) *byte {
 	return &s
 }
