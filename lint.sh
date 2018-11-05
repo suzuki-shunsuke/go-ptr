d340e5dee526f2625f5e0ddd63dce22bd11574ea
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-11-05 11:09:44 +0900

    build: improve lint

--- lint.sh
+++
@@ -1,6 +0,0 @@
-echo "+ gofmt"
-(! go list -f "{{.Dir}}" ./... | xargs gofmt -s -d | grep '^') || exit 1
-echo "+ gometalinter"
-gometalinter ./... || exit 1
-echo "+ staticcheck (failure is ignored)"
-staticcheck ./... || echo "staticcheck failure is ignored"
